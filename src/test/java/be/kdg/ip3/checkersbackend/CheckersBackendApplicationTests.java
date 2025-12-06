c7a898a6311ce6754e9fc3a3d07bea01c3b987b2
Pieter Neyt <pieter.neyt@student.kdg.be>
2025-12-06 21:28:28 +0100

    jopelijk fiks pipeline

--- src/test/java/be/kdg/ip3/checkersbackend/CheckersBackendApplicationTests.java
+++ src/test/java/be/kdg/ip3/checkersbackend/CheckersBackendApplicationTests.java
@@ -2,12 +2,14 @@ package be.kdg.ip3.checkersbackend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class CheckersBackendApplicationTests {
 
     @Test
     void contextLoads() {
     }
-
 }
+
