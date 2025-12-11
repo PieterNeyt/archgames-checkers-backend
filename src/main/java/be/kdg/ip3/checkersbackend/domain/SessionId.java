fe8d9aa3453e725e5ae6db02e71db61efe825f91
Pieter Neyt <pieter.neyt@student.kdg.be>
2025-12-11 17:03:44 +0100

    sessionid updated

--- src/main/java/be/kdg/ip3/checkersbackend/domain/SessionId.java
+++ src/main/java/be/kdg/ip3/checkersbackend/domain/SessionId.java
@@ -1,10 +1,17 @@
 package be.kdg.ip3.checkersbackend.domain;
 
+import org.springframework.util.Assert;
+
 import java.util.UUID;
 
 public record SessionId(UUID id) {
-    public static SessionId generate() {
+    public SessionId {
+        Assert.notNull(id, "id is null");
+    }
+    public static SessionId create() {
         return new SessionId(UUID.randomUUID());
     }
-
+    public NotFoundException notFound() {
+        return new NotFoundException("Game [" + id + "] not found");
+    }
 }
